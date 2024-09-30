670a9df81bff3f91555b6343e6741b911c66ba3a
ACG\Nikhil.Narangalat <nikhil.narangalat@vensure.com>
2024-09-30 12:01:14 +0530

    changes

--- SkillVault.service rest api fomat/SlotEase.Application/DTO/User/UserRequestDto.cs
+++ SkillVault.service rest api fomat/SlotEase.Application/DTO/User/UserRequestDto.cs
@@ -8,8 +8,8 @@ namespace SlotEase.Application.DTO.User
 {
     public  class UserRequestDto
     {
-            public int pageNumber { get; set; }
-            public int pageSize { get; set; }
+        public int pageNumber { get; set; }
+        public int pageSize { get; set; }
         public string email { get; set; } = string.Empty;
      
     }
